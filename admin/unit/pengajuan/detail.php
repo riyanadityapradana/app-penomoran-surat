518d7f41f9a924aa2eb822a508eb6a29fcc6db9e
riyanadityapradana <riyanadityapradanaa@gmail.com>
2026-06-04 10:10:49 +0800

    menambahkan fitur edit data di detail pengajuan

--- admin/unit/pengajuan/detail.php
+++ admin/unit/pengajuan/detail.php
@@ -94,6 +94,31 @@ if (isset($_POST['upload_pdf'])) {
     }
 }
 
+if (isset($_POST['edit_nomor_dokumen'])) {
+    $nomor_surat = isset($_POST['nomor_surat']) ? trim($_POST['nomor_surat']) : '';
+
+    if ($nomor_surat === '') {
+        header('Location: main_admin.php?unit=detail_pengajuan&id_pengajuan=' . $id_pengajuan . '&err=Nomor dokumen tidak boleh kosong!');
+        exit;
+    }
+
+    $nomor_surat = mysqli_real_escape_string($config, $nomor_surat);
+    $update_nomor = mysqli_query($config, "
+        UPDATE tb_pengajuan_dokumen
+        SET nomor_surat = '$nomor_surat'
+        WHERE id_pengajuan = '$id_pengajuan'
+    ");
+
+    if ($update_nomor) {
+        header('Location: main_admin.php?unit=detail_pengajuan&id_pengajuan=' . $id_pengajuan . '&msg=Nomor dokumen berhasil diperbarui!');
+        exit;
+    }
+
+    $errMsg = urlencode('Gagal memperbarui nomor dokumen: ' . mysqli_error($config));
+    header('Location: main_admin.php?unit=detail_pengajuan&id_pengajuan=' . $id_pengajuan . '&err=' . $errMsg);
+    exit;
+}
+
 function getBadgeClass($status) {
     switch ($status) {
         case 'Menunggu Verifikasi': return 'badge badge-warning';
@@ -189,6 +214,9 @@ function getBadgeClass($status) {
                             <?php else: ?>
                                 <span class="badge badge-danger" style="font-size:1rem;">Belum ada nomor</span>
                             <?php endif; ?>
+                            <button type="button" class="btn btn-sm btn-warning ml-2" data-toggle="modal" data-target="#modalEditNomorDokumen">
+                                <i class="fas fa-edit"></i> Edit
+                            </button>
                         </td>
                     </tr>
                     <tr>
@@ -211,6 +239,33 @@ function getBadgeClass($status) {
                     </tr>
                     <?php endif; ?>
                 </table>
+
+                <div class="modal fade" id="modalEditNomorDokumen" tabindex="-1" role="dialog" aria-labelledby="modalEditNomorDokumenLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <form method="POST">
+                                <div class="modal-header bg-warning">
+                                    <h5 class="modal-title" id="modalEditNomorDokumenLabel">Edit Nomor Dokumen</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="form-group">
+                                        <label for="nomor_surat">Nomor Dokumen</label>
+                                        <input type="text" name="nomor_surat" id="nomor_surat" class="form-control" value="<?= htmlspecialchars($data['nomor_surat']); ?>" required>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Batal</button>
+                                    <button type="submit" name="edit_nomor_dokumen" class="btn btn-warning">
+                                        <i class="fas fa-save"></i> Simpan
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
 
                 <hr>
 
